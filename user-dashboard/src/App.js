09ebdbf8371a953d1c403f153a9749e9e2f22dcf
Rita-12397 <rhategekimana920@daviscollege.com>
2021-01-08 12:00:56 +0200

    first commit

---
+++ user-dashboard/src/App.js
@@ -0,0 +1,115 @@
+import './App.css';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+import HomeIcon from '@material-ui/icons/Home';
+import PostAddIcon from '@material-ui/icons/PostAdd';
+import SaveIcon from '@material-ui/icons/Save';
+import AppsIcon from '@material-ui/icons/Apps';
+import MenuIcon from '@material-ui/icons/Menu';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import SettingsApplicationsIcon from '@material-ui/icons/SettingsApplications';
+function App() {
+  return (
+    <div className="main1-div">
+      <div className="icons">
+        <HomeIcon/>
+        <PostAddIcon/>
+        <SaveIcon/>
+        <SettingsApplicationsIcon/>
+      </div>
+    <div className="main-div">
+    <div className='main-header'>
+      <h4>Executive Dashborad</h4>
+      <div className='sub-header'>
+      <p className='select'>Select</p>
+      <p className='exec-dash'>Executive Dashborad < ArrowDropDownIcon/></p>
+      <AppsIcon/>
+      <MenuIcon/>
+      </div>
+    </div>
+    <table>
+  <tr className="header-row">
+    <td>Email</td>
+    <td>IP Adress</td>
+    <td>Name</td>
+    <td>Action</td>
+  </tr>
+</table>
+<div className="div-data">
+  <button className='button'>View</button>
+  <div className='sub-div'>
+    <p className='username'>Rita</p>
+    <p className='email'>hrita@gmail.com</p>
+  </div>
+  <p className='p1'>830039485</p>
+  <p className='p2'><AccountCircleIcon/> hateg Rita</p>
+  <p className='p3'><DeleteIcon/> <EditIcon/> </p>  
+</div>
+<div className="div-data">
+  <button className='button'>View</button>
+  <div className='sub-div'>
+    <p className='username'>Rita</p>
+    <p className='email'>hrita@gmail.com</p>
+  </div>
+  <p className='p1'>830039485</p>
+  <p className='p2'>hateg Rita</p>
+  <p className='p3'><DeleteIcon/> <EditIcon/> </p>  
+</div>
+<div className="div-data">
+  <button className='button'>View</button>
+  <div className='sub-div'>
+    <p className='username'>Rita</p>
+    <p className='email'>hrita@gmail.com</p>
+  </div>
+  <p className='p1'>830039485</p>
+  <p className='p2'>hateg Rita</p>
+  <p className='p3'><DeleteIcon/> <EditIcon/> </p>  
+</div>
+<div className="div-data">
+  <button className='button'>View</button>
+  <div className='sub-div'>
+    <p className='username'>Rita</p>
+    <p className='email'>hrita@gmail.com</p>
+  </div>
+  <p className='p1'>830039485</p>
+  <p className='p2'>hateg Rita</p>
+  <p className='p3'><DeleteIcon/> <EditIcon/> </p>  
+</div>
+<div className="div-data">
+  <button className='button'>View</button>
+  <div className='sub-div'>
+    <p className='username'>Rita</p>
+    <p className='email'>hrita@gmail.com</p>
+  </div>
+  <p className='p1'>830039485</p>
+  <p className='p2'>hateg Rita</p>
+  <p className='p3'><DeleteIcon/> <EditIcon/> </p>  
+</div>
+<div className="div-data">
+  <button className='button'>View</button>
+  <div className='sub-div'>
+    <p className='username'>Rita</p>
+    <p className='email'>hrita@gmail.com</p>
+  </div>
+  <p className='p1'>830039485</p>
+  <p className='p2'>hateg Rita</p>
+  <p className='p3'><DeleteIcon/> <EditIcon/> </p>  
+</div>
+<div className="div-data">
+  <button className='button'>View</button>
+  <div className='sub-div'>
+    <p className='username'>Rita</p>
+    <p className='email'>hrita@gmail.com</p>
+  </div>
+  <p className='p1'>830039485</p>
+  <p className='p2'>hateg Rita</p>
+  <p className='p3'><DeleteIcon/> <EditIcon/> </p>  
+</div>
+</div>
+</div>
+ );
+}
+
+export default App;
